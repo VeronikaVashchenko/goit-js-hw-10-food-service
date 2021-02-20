c448cc10b1adf67df4eea8df457240cddec2c3db
VeronikaVashchenko <69722285+VeronikaVashchenko@users.noreply.github.com>
2021-02-20 13:39:52 +0200

    додала файли

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import './styles.css';
+import template from './gallery-items.hbs';
+import menu from './menu.json';
+
+const refs = {
+  menuList: document.querySelector('.js-menu'),
+};
+
+const markUp = template(menu);
+refs.menuList.insertAdjacentHTML('beforeend', markUp);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const changeTheme = document.querySelector('#theme-switch-toggle');
+
+changeTheme.addEventListener('change', savedChangeTheme);
+const themeShow = document.querySelector('body');
+
+function defaultTheme() {
+  if (localStorage.getItem('Theme') === Theme.DARK) {
+    themeShow.classList.add(Theme.DARK);
+    changeTheme.checked = true;
+    themeShow.classList.remove(Theme.LIGHT);
+  } else {
+    localStorage.setItem('Theme', 'light-theme');
+    themeShow.classList.add(Theme.LIGHT);
+  }
+}
+defaultTheme();
+
+function savedChangeTheme(e) {
+  console.log(e);
+  if (e.target.checked) {
+    themeShow.classList.add(Theme.DARK);
+    themeShow.classList.remove(Theme.LIGHT);
+    localStorage.setItem('Theme', 'dark-theme');
+  } else if (!e.target.checked) {
+    themeShow.classList.add(Theme.LIGHT);
+    themeShow.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', 'light-theme');
+  }
+}
